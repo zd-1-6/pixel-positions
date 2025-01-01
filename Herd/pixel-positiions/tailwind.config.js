7e99ffd9e50afe8316f7daacb2238e5d32ae0aa7
Z.D. <zd.1.6.abb@gmail.com>
2025-01-01 16:15:30 +0200

    Day 27 - from design to blade

---
+++ Herd/pixel-positiions/tailwind.config.js
@@ -0,0 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "black": "#060606"
+            },
+        },
+    },
+    plugins: [],
+};
