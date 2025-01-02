640f1cbb00f18745eead5f7908238d02c9eeb88b
Z.D. <zd.1.6.abb@gmail.com>
2025-01-02 21:47:50 +0200

    Day 29 - Jobs, Tags, TDD, Oh My!

--- Herd/pixel-positiions/app/Models/User.php
+++ Herd/pixel-positiions/app/Models/User.php
@@ -9,13 +9,12 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -26,7 +25,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -44,5 +43,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function employer()
+    {
+        return $this->hasOne(Employer::class);
     }
 }
